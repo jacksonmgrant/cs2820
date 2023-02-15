52caa5e6dd22390e6126c7021e29c654a4a9c561
nsingh5 <nalini-singh@uiowa.edu>
2023-02-15 12:51:14 -0600

    create workloadtest

---
+++ src/edu/uiowa/cs/warp/WorkLoadTest.java
@@ -0,0 +1,89 @@
+package edu.uiowa.cs.warp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WorkLoadTest {
+
+	@Test
+	void testAddFlow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddNodeToFlow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFlowPriorityStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetFlowPriority() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetFlowDeadline() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFlowDeadline() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFlowTxAttemptsPerLink() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetFlowsInRMorder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNodeNamesOrderedAlphabetically() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFlowNames() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNodeIndex() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNodesInFlow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetHyperPeriod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTotalTxAttemptsInFlow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNumTxAttemptsPerLink() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMaxFlowLength() {
+		fail("Not yet implemented");
+	}
+
+}
