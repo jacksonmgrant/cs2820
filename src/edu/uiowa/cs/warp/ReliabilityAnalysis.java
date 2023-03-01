ed04e6940409f10e32a09f9e3309a7f91f44ee94
jgrant6 <jgrant6@uiowa.edu>
2023-03-01 13:08:55 -0600

    Finishing Reliability

--- src/edu/uiowa/cs/warp/ReliabilityAnalysis.java
+++ src/edu/uiowa/cs/warp/ReliabilityAnalysis.java
@@ -39,17 +39,17 @@ package edu.uiowa.cs.warp;
  *
  */
 public class ReliabilityAnalysis {
-  // TODO Auto-generated class
-public ReliabilityTable getReliabilities() {
-      // TODO implement this operation
-      throw new UnsupportedOperationException("not implemented");
-   }
-   
+  // TODO Auto-generated class   
 
 
   public ReliabilityAnalysis(Program program) {
     // TODO Auto-generated constructor stub
   }
+
+public ReliabilityTable getReliabilities() {
+      // TODO implement this operation
+      throw new UnsupportedOperationException("not implemented");
+   }
 
   public Boolean verifyReliabilities() {
     // TODO Auto-generated method stub
