4f6d5d5a87e3cfd8db81c9a1ca4df48edbb12157
jgrant6 <jacksonmgrant@gmail.com>
2023-04-12 11:04:08 -0500

    Autogenerated ReliabilityVisualizationTest

---
+++ src/edu/uiowa/cs/warp/ReliabilityVisualizationTest.java
@@ -0,0 +1,50 @@
+package edu.uiowa.cs.warp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ReliabilityVisualizationTest {
+
+	@BeforeEach
+	void setUp() {
+		//Set up testing objects used in all tests here
+	}
+	
+	//This method may or may not be overridden from VisualizationObject. We should
+	//still test it, but write the other tests first, then write it to the
+	//VisualizationObject method if we haven't figured it out yet.
+	@Test
+	void testVisualization() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreateHeader() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreateColumnHeader() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreateVisualizationData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreateTitle() {
+		fail("Not yet implemented");
+	}
+
+	//This one is implemented in VisualizationObject, may be an easy way to get
+	//file data but wait to implement until we check if we need it.
+	@Test
+	void testCreateFile() {
+		fail("Not yet implemented");
+	}
+
+}
