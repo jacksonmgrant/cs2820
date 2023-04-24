7595fcdc30a74c555693385f0c51714f0f95976e
bnsh <mattboenish@gmail.com>
2023-04-24 15:53:09 -0500

    ReliabilityAnalasisTest BeforeEach

--- src/edu/uiowa/cs/warp/ReliabilityAnalysisTest.java
+++ src/edu/uiowa/cs/warp/ReliabilityAnalysisTest.java
@@ -2,10 +2,25 @@ package edu.uiowa.cs.warp;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import edu.uiowa.cs.warp.SystemAttributes.ScheduleChoices;
 
 class ReliabilityAnalysisTest {
 
+	private WorkLoad workload;
+	private Program program;
+	private WarpInterface warp;
+	private ReliabilityVisualization tester;
+	
+	@BeforeEach
+	void setUp() {
+		workload = new WorkLoad(0, 0.8, 0.99, "Example1a.txt");
+		warp = SystemFactory.create(workload, 16, ScheduleChoices.PRIORITY);
+		tester = new ReliabilityVisualization(warp);
+	}
+	
 	@Test
 	void test() {
 		fail("Not yet implemented");
